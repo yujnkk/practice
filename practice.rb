c327f6b2585b7f6108647eb698ab6e7e0257591e
yujnkk <80437600@qq.com>
2016-09-02 01:25:25 +0800

    tmp log save
    rt

--- practice.rb
+++ practice.rb
@@ -35,3 +35,11 @@ nums = [2,3,1,2,4,3]
 s = 7
 
 p funcOn nums,s
+
+
+Sep  2 01:23:25 melody-Aspire-4741 pptpd[32009]: CTRL: EOF or bad error reading ctrl packet length.
+Sep  2 01:23:25 melody-Aspire-4741 pptpd[32009]: CTRL: couldn't read packet header (exit)
+Sep  2 01:23:25 melody-Aspire-4741 pptpd[32009]: CTRL: CTRL read failed
+Sep  2 01:23:25 melody-Aspire-4741 pptpd[32009]: CTRL: Reaping child PPP[32011]
+Sep  2 01:23:25 melody-Aspire-4741 pptpd[32009]: CTRL: Client 14.215.40.116 control connection finished
+
